46ddc9304e1f618a3ab92726d8465bd958fef185
Andy Ge <andyshge@gmail.com>
2024-11-22 16:35:43 -0500

    initial commit

---
+++ Kanbas/Database/grades.js
@@ -0,0 +1,66 @@
+export default [
+    {
+      "assignmentId": "A101",
+      "studentId": "234",
+      "grade": 95,
+      "feedback": "Excellent analysis and well-documented methods.",
+      "submittedDate": "2024-10-30"
+    },
+    {
+      "assignmentId": "A102",
+      "studentId": "456",
+      "grade": 88,
+      "feedback": "Good effort, but some errors in calculations.",
+      "submittedDate": "2024-11-14"
+    },
+    {
+      "assignmentId": "A103",
+      "studentId": "567",
+      "grade": 92,
+      "feedback": "Innovative design and thorough performance analysis.",
+      "submittedDate": "2024-11-28"
+    },
+    {
+      "assignmentId": "A201",
+      "studentId": "234",
+      "grade": 78,
+      "feedback": "Review aerodynamic principles for better performance.",
+      "submittedDate": "2024-10-19"
+    },
+    {
+      "assignmentId": "A202",
+      "studentId": "456",
+      "grade": 85,
+      "feedback": "Accurate flow analysis but missed a few key points.",
+      "submittedDate": "2024-11-04"
+    },
+    {
+      "assignmentId": "A203",
+      "studentId": "890",
+      "grade": 90,
+      "feedback": "Well-structured evaluation with excellent insights.",
+      "submittedDate": "2024-11-18"
+    },
+    {
+      "assignmentId": "A301",
+      "studentId": "456",
+      "grade": 98,
+      "feedback": "Outstanding structural design with precise calculations.",
+      "submittedDate": "2024-11-29"
+    },
+    {
+      "assignmentId": "A302",
+      "studentId": "234",
+      "grade": 91,
+      "feedback": "Thorough orbital calculations and detailed analysis.",
+      "submittedDate": "2024-12-08"
+    },
+    {
+      "assignmentId": "A303",
+      "studentId": "890",
+      "grade": 89,
+      "feedback": "Comprehensive understanding of systems engineering.",
+      "submittedDate": "2024-12-13"
+    }
+  ];
+  
---
+++ Kanbas/Enrollments/routes.js
@@ -0,0 +1,18 @@
+import * as enrollmentsDao from "./dao.js";
+export default function EnrollmentRoutes(app) {
+    app.get("/api/enrollments/:userId", async (req, res) => {
+        const { userId } = req.params;
+        const enrollments = await enrollmentsDao.getEnrollmentsForUser(userId);
+        res.send(enrollments);
+    });
+    app.post("/api/enrollments", async (req, res) => {
+        const { user, course } = req.body;
+        const newEnrollment = await enrollmentsDao.enrollUserInCourse(user, course);
+        res.send(newEnrollment);
+    });
+    app.delete("/api/enrollments/:enrollmentId", async (req, res) => {
+        const { enrollmentId } = req.params;
+        const status = await enrollmentsDao.deleteEnrollment(enrollmentId);
+        res.send(status);
+    });
+}
