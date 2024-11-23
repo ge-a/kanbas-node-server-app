37d159ed5f05d8163ea19b12c84c09ca92b20a3e
Andy Ge <andyshge@gmail.com>
2024-11-22 20:22:45 -0500

    change index

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 import express from 'express'
-import Hello from "./Hello.js"
-import Lab5 from "./Lab5/index.js";
+import Hello from "./Hello.js";
 import cors from "cors";
-import UserRoutes from './Kanbas/Users/routes.js';
-import CourseRoutes from "./Kanbas/Courses/routes.js";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
-import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
+import Lab5 from "./Lab5/index.js";
+import UserRoutes from "./Kanbas/Users/routes.js";
 import "dotenv/config";
-import session from 'express-session';
+import session from "express-session";
+import CourseRoutes from './Kanbas/Courses/routes.js';
+import ModuleRoutes from './Kanbas/Modules/routes.js';
+import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
+
 const app = express();
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    })
+);
+
 app.use(express.json());
-app.use(cors({
-    credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000", 
-}));
+
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kanbas",
     resave: false,
     saveUninitialized: false,
 };
+
 if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
@@ -29,11 +35,11 @@ if (process.env.NODE_ENV !== "development") {
     };
 }
 app.use(session(sessionOptions));
-Lab5(app);
-Hello(app);
 UserRoutes(app);
+Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 EnrollmentRoutes(app);
+Lab5(app);
 app.listen(process.env.PORT || 4000)
